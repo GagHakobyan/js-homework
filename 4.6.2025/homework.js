ee70f36b1cd24541bc3eba1adf5312763e1a8299
GagHakobyan <hakobyangagik02@mail.ru>
2025-06-04 14:46:47 +0400

    Added homework for 4.6.2025

--- 4.6.2025/homework.js
+++ 4.6.2025/homework.js
@@ -3,3 +3,21 @@ function countCharacters(str) {
 }
 console.log(countCharacters("Hello"));       
 
+function MytoUpperCase(str) {
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    let code = str.charCodeAt(i);
+
+    // Եթե փոքրատառ ա (ASCII 'a'–'z' → 97–122)
+    if (code >= 97 && code <= 122) {
+      code = code - 32; // Մեծատառ դառնալու համար
+    }
+
+    result += String.fromCharCode(code);
+  }
+
+  return result;
+}
+
+console.log(MytoUpperCase("hello")); 
